dcb91d0fd194231a30ab1dc2f6aa79fb9041da61
regina-kim <hlbrie21@gmail.com>
2022-06-14 20:54:14 +0900

    upload server files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/products");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+mongoose
+  .connect(process.env.MONGO_URL.toString())
+  .then(() => console.log("DB connection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(5001, () => {
+  console.log("Backend server is running!");
+});
